dcbbbc45fbed1c8c636050bb3e7904e35b3e4c3c
mtmcatee <102536287+mtmcatee@users.noreply.github.com>
2022-03-28 11:28:06 -0500

    Update Shutdown.ps1

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -10,7 +10,7 @@ $taskAction = New-ScheduledTaskAction `
     -Argument 'Stop-Computer -Force'
 
 #Create task trigger
-$taskTrigger = New-ScheduledTaskTrigger -Daily -At 11:30AM
+$taskTrigger = New-ScheduledTaskTrigger -Daily -At 11:35AM
 
 # Register the new PowerShell scheduled task
 # Register the scheduled task
