705baf851fcff58bc0262323fd5385a1398e37fd
mtmcatee <102536287+mtmcatee@users.noreply.github.com>
2022-03-28 12:57:41 -0500

    Update Shutdown.ps1

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -1,5 +1,5 @@
 # Set the scheduled task time and repitition
-$TaskTime = New-ScheduledTaskTrigger -Daily -At 12:57PM
+$TaskTime = New-ScheduledTaskTrigger -Daily -At 10:00PM
 
 # Set  the task to run as a local administrator with highest level privileges
 $TaskUser = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
@@ -8,4 +8,4 @@ $TaskUser = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLev
 $Action1 = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'Stop-Computer -Force'
 
 # Registers the task with Task Scheduler
-Register-ScheduledTask "Test Scheduled Task" -Action $Action1 -Principal $TaskUser -Trigger $TaskTime
+Register-ScheduledTask "Shutdown" -Action $Action1 -Principal $TaskUser -Trigger $TaskTime
