006e55e8d98f82ccacf1c45ab7f8a270c50c8859
mtmcatee <102536287+mtmcatee@users.noreply.github.com>
2022-03-28 12:42:17 -0500

    Update Shutdown.ps1

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -1,5 +1,5 @@
 # Set the scheduled task time and repitition
-$TaskTime = New-ScheduledTaskTrigger -Daily -At 12:00
+$TaskTime = New-ScheduledTaskTrigger -Daily -At 12:50PM
 
 # Set  the task to run as a local administrator with highest level privileges
 $TaskUser = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
