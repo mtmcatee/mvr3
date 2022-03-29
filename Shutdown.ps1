18052a3b08be7e6aeb6b9b9f7ab564663b045fff
mtmcatee <102536287+mtmcatee@users.noreply.github.com>
2022-03-29 08:21:39 -0500

    Update Shutdown.ps1

--- Shutdown.ps1
+++ Shutdown.ps1
@@ -2,7 +2,7 @@
 $TaskTime = New-ScheduledTaskTrigger -Daily -At 8:21AM
 
 # Set  the task to run as a local administrator with highest level privileges
-$TaskUser = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest 
+$TaskUser = New-ScheduledTaskPrincipal -GroupId "NT AUTHORITY\SYSTEM" -RunLevel Highest 
 
 # Set actions the schedule task should perform
 $Action1 = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'Stop-Computer -Force'
