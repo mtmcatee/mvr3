ff658275e76da21cc21d0685098692cd39463b03
mtmcatee <102536287+mtmcatee@users.noreply.github.com>
2022-04-04 14:29:43 -0500

    Update login.ps1

--- login.ps1
+++ login.ps1
@@ -5,3 +5,4 @@ Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 Set-ItemProperty $RegPath "AutoLogonCount" -Value "1" -type DWord
+Start-Sleep -Seconds 5
